bfc2ba9ab1af6c1e63d481e880c8db0acc178902
Maulana Akmal <maulana2904@gmail.com>
2018-04-20 22:50:16 +0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tweepy
 
 auth = tweepy.OAuthHandler('e3Qzanw0bnvTbunxOLr6GM2vi', 'IrBFKeNwfup4SRJDRtxwKsHJMRg5sppk6GD85GnWEKQ6b2Vfnk')
+api = tweepy.API(auth)
 auth.set_access_token('1974502530-UIWm0t1pIzQMbmYgacEbrAf3TYKe1iMKn8op5hJ', 'KSCFR1kdXeESS4GrOfC37obNplr2wLRbmmhcBBgP69rnu')
 
-api = tweepy.API(auth)
+class MyStreamListener(tweepy.StreamListener):
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet
-    break
+    def on_status(self, status):
+        print(status.text)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream.filter(track=[])
+
